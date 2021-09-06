1f9616f7dba312bbd50bfa2a8a6d12eb14891157
Umron <87381360+Umronn@users.noreply.github.com>
2021-09-06 20:23:02 +0700

    Add files via upload

---
+++ Folder Proyek/icm.js
@@ -0,0 +1,8 @@
+function klik() {
+  let jawaban = prompt("haloo, selamat datang di web ICM, mau tau lebih lanjut?");
+  if ((jawaban = "mau")) {
+    alert("Silahkan baca webnya yaa");
+  } else {
+    alert("yaudah kalau gamau");
+  }
+}
